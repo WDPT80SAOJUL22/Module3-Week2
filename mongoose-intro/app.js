7d2f3322edfb0586cd93bf6451fe5b62769ef12e
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-10-27 22:21:14 -0300

    schemas

--- mongoose-intro/app.js
+++ mongoose-intro/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 
 // require('db/inde.js')
 import './db/index.js'
-
 import Cat from './models/Cat.model.js'
 
 const app = express()
@@ -18,10 +17,32 @@ app.get('/', (req, res) => {
 //   )
 // })
 
+app.get('/find', async (req, res) => {
+  //   let cat = await Cat.find()
+  //   let cat = await Cat.findById('635b1ba707a2fa15bf69639b')
+  //   let cat = await Cat.findOne({ name: 'Matilde' })
+  //   let cat = await Cat.findByIdAndUpdate('635b1ba707a2fa15bf69639b', {
+  //     name: 'Iury',
+  //   })
+  let cat = await Cat.findByIdAndDelete('635b1ba707a2fa15bf69639b')
+
+  res.json(cat)
+})
+
 app.get('/:name', async (req, res) => {
   const { name } = req.params
-  const newCat = await Cat.create({ name: name, age: 5, color: 'blue' })
-  res.json(newCat)
+  try {
+    const newCat = await Cat.create({
+      name: name,
+      age: 5,
+      color: 'blue',
+      email: 'carlos@carlos.com',
+      linkedin: 'https://linkedin.com/123',
+    })
+    res.json(newCat)
+  } catch (err) {
+    res.json(err.message)
+  }
 })
 
 app.listen(5000, () => {
--- mongoose-intro/models/Cat.model.js
+++ mongoose-intro/models/Cat.model.js
@@ -1,10 +1,43 @@
 import { Schema, model } from 'mongoose'
 
-const catSchema = new Schema({
-  name: String,
-  age: Number,
-  color: String,
-})
+const catSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      maxlenght: 20,
+      minlenght: 3,
+      trim: true,
+    },
+    age: { type: Number, max: 20, min: 3 },
+    color: { type: String, enum: ['blue', 'green', 'red'] },
+    image: {
+      type: String,
+      default:
+        'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg',
+    },
+    email: {
+      type: String,
+      match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+    },
+    linkedin: {
+      type: String,
+      validate: {
+        validator: (text) => {
+          return text.indexOf('https://linkedin.com/') === 0
+        },
+        message: 'linkedin profile must starts with https://linkedin.com/',
+      },
+    },
+    //   birth: Date, // ('2022-01-21')
+    //   neutered: Boolean,
+    //   toys: [String],
+    //   anything: Schema.Types.Mixed,
+    //   tutor: Schema.Types.ObjectId,
+  },
+  { timestamps: true }
+)
 
 const Cat = model('Cat', catSchema)
 
